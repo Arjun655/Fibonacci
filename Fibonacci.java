07a76b2599efce32a8a85a6d959509551b6d51fc
Arjun655 <53296273+Arjun655@users.noreply.github.com>
2021-12-02 13:19:41 +0530

    Add files via upload
    Fibonacci by java

---
+++ Fibonacci.java
@@ -0,0 +1,45 @@
+//package Fibonacci;
+
+import java.util.Scanner;
+
+public class Fibonacci
+
+{
+    
+    public static void main(String []args)
+
+  {
+    
+   int i,c=0,n;
+   
+   Scanner sc=new Scanner(System.in);
+    
+    System.out.println("Enter a number to generate Fibonacci series upto nth term ");
+   
+     n=sc.nextInt();
+   
+    int a=0;
+   
+    int b=1;
+   
+    System.out.println("Fibonacci series upto "+n+" is: ");
+  
+     while(c<=n)
+   
+     {
+        
+       System.out.println(c+" ");
+    
+   
+        a=b;
+      
+        b=c;
+    
+        c=a+b;
+   
+     }
+ 
+   
+   }
+
+} 
